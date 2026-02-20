b16be61581138968e4ab9c970c6f9e6a92948801
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-02-20 09:18:26 +0530

    AllDivisor using vector and less time complexity

--- BasicMath/AllDivisors.cpp
+++ BasicMath/AllDivisors.cpp
@@ -3,10 +3,10 @@
 using namespace std;
 
 int divisor (int n){
-    
-    for (int i; i<n; i++){
+    cout << "Divisors are: " << " ";
+    for (int i = 1; i<=n; i++){
         if (n%i == 0){
-            cout << i;
+            cout << i << " ";
         }
     }
 }
@@ -24,18 +24,19 @@ int main() {
 
 
 
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<int> divisor(int n) {
     vector<int> divs;
-
+    
     for (int i = 1; i <= n; i++) {
         if (n % i == 0) {
             divs.push_back(i);
         }
     }
-
+    
     return divs;
 }
 
@@ -43,13 +44,48 @@ int main() {
     int number;
     cout << "Enter the Number: ";
     cin >> number;
-
+    
     vector<int> result = divisor(number);
-
+    
     cout << "Divisors are: ";
     for (int d : result) {
         cout << d << " ";
     }
+    
+    return 0;
+}
+*/
 
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void divisor (int n){
+    vector<int> ls;
+    for (int i= 1; i<=sqrt(n); i++){
+        if (n%i == 0){
+            ls.push_back(i);
+            if ((n/i) != i){
+                ls.push_back(n/i);
+            }
+        }
+    }
+    sort(ls.begin(), ls.end());
+    
+    cout << "Divisors are: ";
+    for(auto it: ls)
+    cout << it << " ";
+}
+
+
+int main() {
+    int n;
+    cout << "Enter the Number: ";
+    cin >> n;
+    
+    divisor(n);
     return 0;
 }
