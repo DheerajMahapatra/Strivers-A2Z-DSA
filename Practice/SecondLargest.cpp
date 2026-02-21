543868777d93d1753890058b386c4a6b53cc4c91
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-02-21 19:30:55 +0530

    SecondLargest,CheckTheArrayIsSorted

--- Practice/SecondLargest.cpp
+++ Practice/SecondLargest.cpp
@@ -21,3 +21,35 @@ int main(){
 
 
 
+
+
+
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int secondLargest(vector<int> arr)
+{
+    int large = INT_MIN, secondLarge = INT_MIN;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > large)
+        {
+            secondLarge = large;
+            large = arr[i];
+        }
+        else if (arr[i] > secondLarge && arr[i] != large)
+            secondLarge = arr[i];
+    }
+    return secondLarge;
+}
+
+int main()
+{
+    vector<int> arr = {3, 2, 1, 6, 0, 7};
+    cout << secondLargest(arr);
+}
