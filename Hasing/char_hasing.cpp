804b5dd199f9d129165db8e8062df621e80e5d24
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-02-26 21:03:11 +0530

    Char_Hasing

---
+++ Hasing/char_hasing.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int hashh[26];   // global declaration (for lowercase letters)
+
+int main(){
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+
+    // precompute
+    for(int i = 0; i < s.size(); i++){
+        hashh[s[i] - 'a']++;
+    }
+
+    int q;
+    cout << "Enter how many characters you want to search: ";
+    cin >> q;
+
+    while(q--){
+        char c;
+        cout << "Enter the character: ";
+        cin >> c;
+
+        // fetch
+        cout << c << " appears : "<< hashh[c - 'a'] << " times." << endl;
+    }
+
+    return 0;
+}
