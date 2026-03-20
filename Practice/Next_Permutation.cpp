1714ab3c2bee7518d278214081d712d1ba5c9bed
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-03-20 14:31:39 +0530

    Next Permutation

---
+++ Practice/Next_Permutation.cpp
@@ -0,0 +1,16 @@
+# include<bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int>& nums) {
+    next_permutation(nums.begin(), nums.end());
+}
+
+int main(){
+    vector<int> nums ={1,2,3};
+    nextPermutation(nums);
+
+    for(auto &val : nums){
+        cout << val << " ";
+    }
+    return 0;
+}
