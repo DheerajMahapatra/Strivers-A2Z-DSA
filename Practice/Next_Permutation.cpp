db89a903935bbff3616fecf3980fb726507d6c83
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-03-23 13:45:06 +0530

    Next Permutation (Optimal)

--- Practice/Next_Permutation.cpp
+++ Practice/Next_Permutation.cpp
@@ -1,3 +1,4 @@
+/*
 # include<bits/stdc++.h>
 using namespace std;
 
@@ -13,4 +14,43 @@ int main(){
         cout << val << " ";
     }
     return 0;
+}
+*/
+
+# include<bits/stdc++.h>
+using namespace std;
+
+void nextPermutation(vector<int> &nums){
+    int n = nums.size();
+    int i;
+
+    // Finding the pivot point
+    for (i = n-2; i>=0; i--){
+        if (nums[i] < nums[i+1]){
+            break;
+        }
+    }
+
+    // Finding the next greater
+    if(i >= 0){
+        for (int j = n-1; j >=0; j--){
+            if (nums[j] > nums[i]){
+                swap (nums[j], nums[i]);
+                break;
+            }
+        }
+    }
+
+    // Reverse=ing the suffix
+    reverse(nums.begin() + i + 1, nums.end());
+
+    // Printing the Next Permutation
+    for (int k = 0; k < n; k++){
+        cout << nums[k] << " ";
+    }
+}
+
+int main(){
+    vector<int> nums = {1,2,3};
+    nextPermutation(nums);
 }
