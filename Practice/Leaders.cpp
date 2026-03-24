e8de5139bda4cc4094f869de1cc4c7d0e4e6efac
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-03-24 13:09:42 +0530

    Leaders (Optimal)

--- Practice/Leaders.cpp
+++ Practice/Leaders.cpp
@@ -1,3 +1,4 @@
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -7,14 +8,14 @@ void leader(vector<int> &nums){
 
     for (int i = 0; i < n; i++){
         bool isLeader = true;
-
+        
         for (int j = i + 1; j < n; j++){
             if (nums[j] > nums[i]){
                 isLeader = false;
                 break;
             }
         }
-
+        
         if (isLeader){
             result.push_back(nums[i]);
         }
@@ -28,5 +29,51 @@ void leader(vector<int> &nums){
 int main(){
     vector<int> nums = {10,22,12,3,0,6};
     leader(nums);
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# include <bits/stdc++.h>
+using namespace std;
+
+vector<int> leaderOfArray(vector<int> &nums){
+        int n = nums.size();
+        int leader = nums[n-1];
+
+        vector<int> result;
+        result.push_back(nums[n-1]);
+
+        for (int i = n -1; i >= 0; i--){
+            if (nums[i] > leader){
+                result.push_back(nums[i]);
+                leader = nums[i];
+            }
+        }
+
+        reverse(result.begin(), result.end());
+        for(auto val : result){
+            cout << val << " ";
+        }
+
+}
+
+
+int main(){
+    vector<int> nums = {10,22,12,3,0,6};
+    leaderOfArray(nums);
     return 0;
 }
