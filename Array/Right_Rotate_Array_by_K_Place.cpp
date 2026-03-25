f702278d08b50d77714108c78c447b34c562de49
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-03-25 09:22:26 +0530

    Right Rotate Array by K Place (Optimal)

--- Array/Right_Rotate_Array_by_K_Place.cpp
+++ Array/Right_Rotate_Array_by_K_Place.cpp
@@ -1,3 +1,73 @@
+/*
+// Brute Force
+# include<bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int>& nums, int k){
+    int n = nums.size();
+    k = k % n;
+    
+    // Store last k elements
+    vector<int> temp(k);
+    for (int i = 0; i < k; i++){
+        temp[i] = nums[n - k +i];
+    }
+    
+    // Shift remaining elements to right
+    for(int i = n - k - 1; i >= 0; i--){
+        nums[i+k] = nums[i];
+    }
+    
+    // Last insert the temporary elements
+    for(int i = 0; i < k; i++){
+        nums[i] = temp[i];
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    
+    vector<int> nums(n);
+    
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> nums[i];
+    }
+    
+    cout << "\n";
+    cout <<"Before Rotate The Array : ";
+    for(int i = 0; i < n; i++){
+        cout << nums[i] << " ";
+    }
+    
+    cout << "\n";
+    int k;
+    cout << "Enter the Rotation Number: ";
+    cin >> k;
+    rotate(nums, k);
+    
+    cout << "\n";
+    cout <<"After Right Rotate The Array : ";
+    for(int i = 0; i < n; i++){
+        cout << nums[i] << " ";
+    }
+    
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+// Optimal
 # include<bits/stdc++.h>
 using namespace std;
 
@@ -5,21 +75,15 @@ void rotate(vector<int>& nums, int k){
     int n = nums.size();
     k = k % n;
 
-    // Store last k elements
-    vector<int> temp(k);
-    for (int i = 0; i < k; i++){
-        temp[i] = nums[n - k +i];
-    }
+    // Step 1: reverse whole array
+    reverse(nums.begin(), nums.end());
 
-    // Shift remaining elements to right
-    for(int i = n - k - 1; i >= 0; i--){
-        nums[i+k] = nums[i];
-    }
+    // Step 2: reverse first k elements
+    reverse(nums.begin(), nums.begin() + k);
 
-    // Last insert the temporary elements
-    for(int i = 0; i < k; i++){
-        nums[i] = temp[i];
-    }
+    // Step 3: reverse remaining
+    reverse(nums.begin() + k, nums.end());
+
 }
 
 int main(){
