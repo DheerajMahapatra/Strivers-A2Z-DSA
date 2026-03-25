ce9fed1b37efb95a765fc80f8781ac62c7a7c7f0
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-03-25 09:15:55 +0530

    Left Rotate Array by K Place (Optimal)

--- Array/Left_Rotate_Array_by_K_Place.cpp
+++ Array/Left_Rotate_Array_by_K_Place.cpp
@@ -45,8 +45,8 @@ int main(){
 
 
 
-
-
+/*
+// Brute Force
 # include<bits/stdc++.h>
 using namespace std;
 
@@ -59,16 +59,74 @@ void rotateArray(vector<int>& nums, int k){
     for (int i = 0; i < k; i++){
         temp[i] = nums[i];
     }
-
+    
     // Shift Elements by K Place
     for(int i = k; i < n; i++){
         nums[i-k] = nums[i];
     }
-
+    
     // Last insert the temporary elements
     for(int i = n-k; i < n; i++){
         nums[i] = temp[i - (n-k)];
     }
+}
+
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    
+    vector<int> nums(n);
+    
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> nums[i];
+    }
+    
+    cout << "\n";
+    cout <<"Before Rotate The Array : ";
+    for(int i = 0; i < n; i++){
+        cout << nums[i] << " ";
+    }
+    
+    cout << "\n";
+    int k;
+    cout << "Enter the Rotation Number: ";
+    cin >> k;
+    rotateArray(nums, k);
+    
+    cout << "\n";
+    cout <<"After Rotate The Array : ";
+    for(int i = 0; i < n; i++){
+        cout << nums[i] << " ";
+    }
+    
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Optimal
+# include<bits/stdc++.h>
+using namespace std;
+
+void rotateArray(vector<int>& nums, int k){
+    int n = nums.size();
+    k = k % n;
+    reverse(nums.begin(), nums.begin() + k);
+    reverse(nums.begin() + k, nums.end());
+    reverse(nums.begin(), nums.end());
 }
 
 int main(){
@@ -103,3 +161,18 @@ int main(){
 
     return 0;
 }
+
+
+
+// // Function to reverse the array
+// void Reverse(int arr[], int start, int end)
+// {
+//     while (start <= end)
+//     {
+//         int temp = arr[start];
+//         arr[start] = arr[end];
+//         arr[end] = temp;
+//         start++;
+//         end--;
+//     }
+// }
