bf2921d822c8a272369af55cd7c16493b5aba977
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-02 22:53:31 +0530

    Intersection Of The Array (Optimal)

--- Array/intersection_of_two_array.cpp
+++ Array/intersection_of_two_array.cpp
@@ -1,10 +1,12 @@
+/*
+// Brute Force
 #include<bits/stdc++.h>
 using namespace std;
 
 vector<int> findArrayIntersection(vector<int> &A, vector<int> &B){
     int n = A.size();
     int m = B.size();
-
+    
     vector<int> ans;
     int vis[m] = {0};
     for (int i = 0; i < n; i++){
@@ -14,11 +16,91 @@ vector<int> findArrayIntersection(vector<int> &A, vector<int> &B){
                 vis[j] = 1;
                 break;
             }
-
+            
             if (B[j] > A[i]) break;
         }
     }
+    
+    return ans;
+}
 
+int main(){
+    int n1;
+    cout << "Enter the size of first array: ";
+    cin >> n1;
+    
+    vector<int> a(n1);
+    for(int i = 0; i < n1; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> a[i];
+    }
+    
+    cout << "\nOriginal First Array: ";
+    for(auto &val : a){
+        cout << val << " ";
+    }
+    
+    cout << "\n\n";
+    
+    int n2;
+    cout << "Enter the size of second array: ";
+    cin >> n2;
+    
+    vector<int> b(n2);
+    for(int i = 0; i < n2; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> b[i];
+    }
+    
+    cout << "\nOriginal Second Array: ";
+    for(auto &val : b){
+        cout << val << " ";
+    }
+    
+    cout << "\n\n";
+    
+    vector<int> result = findArrayIntersection(a, b);
+    
+    cout << "Intersection of Array: ";
+    for(auto &val : result){
+        cout << val << " ";
+    }
+    
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> findArrayIntersection(vector<int> &A, vector<int> &B){
+    int n = A.size();
+    int m = B.size();
+    
+    int i = 0;
+    int j = 0;
+    vector<int> ans;
+    while(i < n && j < m){
+        if (A[i] < B[j]){
+            i++;
+        }
+
+        else if (B[j] < A[i]){
+            j++;
+        }
+
+        else {
+            ans.push_back(A[i]);
+            i++;
+            j++;
+        }
+    }
     return ans;
 }
 
