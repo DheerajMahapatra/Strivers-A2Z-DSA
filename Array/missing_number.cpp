60968a4505ff4c61e3a488698e67389f751ec019
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-03 20:40:03 +0530

    Missing Number (Better)

--- Array/missing_number.cpp
+++ Array/missing_number.cpp
@@ -51,4 +51,53 @@ int main(){
 
 
 
+// Better
+#include <bits/stdc++.h>
+using namespace std;
 
+int missing_number(vector<int> &nums){
+    int n = nums.size();
+
+    vector<int> hash(n+1, 0);
+
+    for(int i = 0; i < n-1; i++){
+            hash[nums[i]] = 1;
+    }
+
+    for(int i = 1; i <= n; i++){
+        if(hash[i] == 0){
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> nums[i];
+    }
+
+    cout << "\nOriginal Array: ";
+    for(auto &val : nums){
+        cout << val << " ";
+    }
+    cout << "\n\n";
+
+    int result = missing_number(nums);
+
+    if(result == -1){
+        cout << "No missing number. ";
+    } else {
+        cout << "Missing number is: " << result;
+    }
+
+    return 0;
+}
