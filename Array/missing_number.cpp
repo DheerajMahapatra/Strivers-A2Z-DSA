9cda5b55005c120741474f820ff95801f0fb54e1
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-03 23:07:09 +0530

    Missing Number (Optimal Using SUM)

--- Array/missing_number.cpp
+++ Array/missing_number.cpp
@@ -112,32 +112,95 @@ int main(){
 
 
 
-
+// Better For Multiple Values
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 vector<int> missing_numbers(vector<int> &nums){
     
-    // Step 1: find max element
-    int maxi = *max_element(nums.begin(), nums.end());
+// Step 1: find max element
+int maxi = *max_element(nums.begin(), nums.end());
 
-    // Step 2: hash array till max
-    vector<int> hash(maxi + 1, 0);
+// Step 2: hash array till max
+vector<int> hash(maxi + 1, 0);
 
-    // Step 3: mark present
-    for(int i = 0; i < nums.size(); i++){
-        hash[nums[i]] = 1;
+// Step 3: mark present
+for(int i = 0; i < nums.size(); i++){
+    hash[nums[i]] = 1;
+}
+
+// Step 4: find missing
+vector<int> missing;
+for(int i = 1; i <= maxi; i++){
+    if(hash[i] == 0){
+        missing.push_back(i);
     }
+}
 
-    // Step 4: find missing
-    vector<int> missing;
-    for(int i = 1; i <= maxi; i++){
-        if(hash[i] == 0){
-            missing.push_back(i);
+return missing;
+}
+
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    
+    for(int i = 0; i < n; i++){
+        cout << "Enter element " << i+1 << ": ";
+        cin >> nums[i];
+    }
+    
+    cout << "\nOriginal Array: ";
+    for(auto &val : nums){
+        cout << val << " ";
+    }
+    cout << "\n\n";
+    
+    vector<int> result = missing_numbers(nums);
+    
+    if(result.empty()){
+        cout << "No missing numbers.";
+    }
+    else{
+        cout << "Missing numbers are: ";
+        for(auto &val : result){
+            cout << val << " ";
         }
     }
+    
+    return 0;
+}
+*/
 
-    return missing;
+
+
+
+
+
+
+
+
+// Optimal Using SUM
+#include <bits/stdc++.h>
+using namespace std;
+
+int missing_number(vector<int> &nums){
+    int n = nums.size();
+
+    // Step 1: Expected sum
+    int expectedSum = ((n+1) * (n + 2)) / 2;
+
+    // Step 2: Actual sum of array
+    int actualSum = 0;
+    for(int i = 0; i < n; i++){
+        actualSum += nums[i];
+    }
+
+    // Step 3: Missing number
+    return expectedSum - actualSum;
 }
 
 int main(){
@@ -158,17 +221,8 @@ int main(){
     }
     cout << "\n\n";
 
-    vector<int> result = missing_numbers(nums);
-
-    if(result.empty()){
-        cout << "No missing numbers.";
-    }
-    else{
-        cout << "Missing numbers are: ";
-        for(auto &val : result){
-            cout << val << " ";
-        }
-    }
+    int result = missing_number(nums);
+    cout << "Missing number is: " << result;
 
     return 0;
 }
