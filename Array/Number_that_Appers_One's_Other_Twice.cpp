5b285382a9515510e41c54f5495866346936b9a0
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-15 23:07:59 +0530

    Number that Appers Ones's Other Twice (Using MAP)

--- Array/Number_that_Appers_One's_Other_Twice.cpp
+++ Array/Number_that_Appers_One's_Other_Twice.cpp
@@ -58,26 +58,82 @@ int main() {
 
 
 // BETTER
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int>& arr) {
+    int n = arr.size();
+    
+    int maxi = arr[0];
+    for(int i = 0; i < n; i++){
+        maxi = max(maxi, arr[i]);
+    }
+    
+    vector<int> hash(maxi + 1, 0);
+    
+    for(int i = 0; i < n; i++){
+        hash[arr[i]]++;
+    }
+    
+    for(int i = 0; i < n; i++){
+        if(hash[arr[i]] == 1){
+            return arr[i];
+        }
+    }
+    
+    return -1;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    
+    vector<int> arr(n);
+    
+    for(int i = 0; i < n; i++){
+        cout << "Enter the " << i+1 << " element: ";
+        cin >> arr[i];
+    }
+    
+    cout << "\nArray: ";
+    for(auto &val : arr){
+        cout << val << " ";
+    }
+    cout << "\n\n";
+    
+    int result = getSingleElement(arr);
+    cout << "Single non-repeating element: " << result;
+    
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+// Using MAP
 #include <bits/stdc++.h>
 using namespace std;
 
 int getSingleElement(vector<int>& arr) {
     int n = arr.size();
 
-    int maxi = arr[0];
+    map<int, int> mpp;
+
     for(int i = 0; i < n; i++){
-        maxi = max(maxi, arr[i]);
+        mpp[arr[i]]++;
     }
 
-    vector<int> hash(maxi + 1, 0);
-
-    for(int i = 0; i < n; i++){
-        hash[arr[i]]++;
-    }
-
-    for(int i = 0; i < n; i++){
-        if(hash[arr[i]] == 1){
-            return arr[i];
+    for(auto it : mpp){
+        if(it.second == 1){
+            return it.first;
         }
     }
 
