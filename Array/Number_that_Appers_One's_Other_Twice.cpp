2013bb3a5f0ad6b2c8e322b22be04db4bd777c50
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-15 23:10:40 +0530

    Number that Appers Ones's Other Twice (OPTIMAL)

--- Array/Number_that_Appers_One's_Other_Twice.cpp
+++ Array/Number_that_Appers_One's_Other_Twice.cpp
@@ -119,28 +119,79 @@ int main() {
 
 
 // Using MAP
+/*
 #include <bits/stdc++.h>
 using namespace std;
 
 int getSingleElement(vector<int>& arr) {
     int n = arr.size();
-
+    
     map<int, int> mpp;
-
+    
     for(int i = 0; i < n; i++){
         mpp[arr[i]]++;
     }
-
+    
     for(auto it : mpp){
         if(it.second == 1){
             return it.first;
         }
     }
-
+    
     return -1;
 }
 
 int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    
+    vector<int> arr(n);
+    
+    for(int i = 0; i < n; i++){
+        cout << "Enter the " << i+1 << " element: ";
+        cin >> arr[i];
+    }
+    
+    cout << "\nArray: ";
+    for(auto &val : arr){
+        cout << val << " ";
+    }
+    cout << "\n\n";
+    
+    int result = getSingleElement(arr);
+    cout << "Single non-repeating element: " << result;
+    
+    return 0;
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+// OPTIMAL
+#include <bits/stdc++.h>
+using namespace std;
+
+int getSingleElement(vector<int>& arr){
+    int xorr = 0;
+
+    for(int i = 0; i < arr.size(); i++){
+        xorr = xorr ^ arr[i];
+    }
+
+    return xorr;
+}
+
+int main(){
     int n;
     cout << "Enter size of array: ";
     cin >> n;
