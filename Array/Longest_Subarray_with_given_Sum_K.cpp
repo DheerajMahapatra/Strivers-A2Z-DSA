8e1d7f9d6291d92a7bf5fe8e420385bccb3b4090
DheerajMahapatra <dheerajmahapatra05@gmail.com>
2026-04-16 09:23:33 +0530

    Longest Subarray With Given Sum k (BRUTE FORCE)

---
+++ Array/Longest_Subarray_with_given_Sum_K.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSubarray(vector<int> &nums, int k)
+{
+    int n = nums.size();
+    int maxLength = 0;
+
+    // starting index
+    for (int startIndex = 0; startIndex < n; startIndex++) {
+        // ending index
+        for (int endIndex = startIndex; endIndex < n; endIndex++) {
+            
+            int currentSum = 0;
+
+            // calculate sum of subarray
+            for (int i = startIndex; i <= endIndex; i++) {
+                currentSum += nums[i];
+            }
+
+            if (currentSum == k)
+                maxLength = max(maxLength, endIndex - startIndex + 1);
+        }
+    }
+    return maxLength;
+}
+
+int main()
+{
+    vector<int> a = { 10, 5, 2, 7, 1, 9};
+    int k = 15;
+
+    int len = longestSubarray(a, k);
+    
+    cout << "The length of the longest subarray is: " << len << "\n";
+    return 0;
+}
